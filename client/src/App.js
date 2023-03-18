20bff95ccbcbdda7fb8669a31e80420e4bbcd1fc
Jasmine Tan <jasmine.tpm@hotmail.com>
2023-03-19 00:59:54 +0800

    edited frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 function App() {
 	return (
 		<div className="App">
-			
+			<h1>Welcome, User</h1>
+            <h4>Your Tasks</h4>
+
+            <div className="todos">
+                <div className="todo">
+                    <div className="checkbox"></div>
+
+                    <div className="text">Get the bread</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+
+                <div className="todo is-complete">
+                    <div className="checkbox"></div>
+
+                    <div className="text">Get the milk</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+            </div>
 		</div>
 	);
 }
